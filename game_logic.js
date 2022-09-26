b2a97626ea087801f096e72c62f84f71ca89eaf2
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-26 20:40:18 +0300

    removed testing code

--- game_logic.js
+++ game_logic.js
@@ -1,9 +1,15 @@
 const tiles = Array.from(document.querySelectorAll('.tile'));
+const current_player = 0;                                    //The player starts by default
 
 tiles.forEach((tile,index) => {
 	tile.addEventListener('click', () => changeTile(tile, index));
 });
 
 const changeTile = (tile, index) => {
+	if(current_player == 0) {                          //computer is yellow user is red
 	tile.style.backgroundImage="url(assets/red.png)";
+	}else{
+		tile.style.backgroundImage="url(assets/yellow.png)";
+	}
 }
+
