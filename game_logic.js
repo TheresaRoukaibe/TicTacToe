3815efe770b27bbad8d337d7285dc062acf2d1f0
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-27 10:50:47 +0300

    changed board logic

--- game_logic.js
+++ game_logic.js
@@ -1,15 +1,17 @@
 const tiles = Array.from(document.querySelectorAll('.tile'));
-const current_player = 0;                                    //The player starts by default
+let current_player = 0;                                    //The player starts by default
 
 tiles.forEach((tile,index) => {
 	tile.addEventListener('click', () => changeTile(tile, index));
 });
 
 const changeTile = (tile, index) => {
-	if(current_player == 0) {                          //computer is yellow user is red
+	if(current_player == 0) {                             //computer is yellow user is red
 	tile.style.backgroundImage="url(assets/red.png)";
+	current_player=1;
 	}else{
 		tile.style.backgroundImage="url(assets/yellow.png)";
+		current_player=0;
 	}
 }
 
